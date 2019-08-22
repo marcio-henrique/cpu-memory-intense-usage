bf2f1a37fa9b441fa82fdf5bca147c0ce0e67b16
Márcio Henrique <ikke.guerreiro@gmail.com>
2019-08-22 18:50:27 -0300

    Modularização do código e adição de comentários

--- src/main.c
+++ src/main.c
@@ -5,67 +5,72 @@
 #include <stdlib.h>
 #include <string.h>
 
-//cat /proc/meminfo | sed -n 2p | awk '{print $2}'
+void father(int pid)
+{
+	char get_cpu[256], get_memory[256], kill[128];
+
+	sprintf(get_memory, "%s%d%s", "pmap ", pid, " | tail -n 1 | awk '{print $2}'");		//string command to show a memory usage in Kb from a process on linux terminal
+	sprintf(get_cpu, "%s%d%s", "ps -p ", pid, " -o %cpu | grep -v CPU ");				//string command to show a UCP usage in % from a process on linux terminal
+	sprintf(kill, "%s%d","kill ",pid);													//string command to kill process on linux
+
+	int i;
+	for (i = 0; i < 10; i++)
+	{	
+		printf("Time: %d second(s)\n", i);
+		printf("Memory Usage (Kb):\n");
+		system(get_memory);					//execute a string command on linux terminal
+		printf("CPU Usage(%%):\n");
+		system(get_cpu);
+		printf("\n");
+
+		sleep(1);	//delay 1 second
+	}
+
+	system(kill);
+}
+
+void child (int arg)
+{
+	if (strcmp(arg, "ucp") == 0) 	//intense UCP utilization
+	{
+		while(1){}
+	}
+	else if (strcmp(arg, "ucp-mem") == 0) 		//intense UCP and principal memory utilization
+	{
+		char *memory = (char *) malloc(sizeof(char));	//memory allocation to char (1 byte)
+		while(1)
+		{
+			if (memory != NULL)		//if memory allocation is available
+			{
+				memory = (char *) malloc(sizeof(char));
+			}
+		}
+	}
+}
+
 int main (int argc, char *argv[], char *envp[]) {
 
-	int pid ; /* identificador de processo */
+	int pid ; 		// process id
 
-	pid = fork () ; /* replicacção do processo */
+	pid = fork () ; // new process created by duplicating the current process
 
-	if ( pid < 0 )
-	{ /* se o fork não funcionou */
-		perror ("Erro: ") ;
-		exit (-1) ; /* encerra o processo com código de erro -1 */ 
+	if ( pid < 0 )	// if fork failed
+	{ 
+		perror ("Error: ") ;		//error status print
+		exit (-1) ; 			// finish the process with a -1 error code
 	}
-
-	if( pid > 0 ) /* se sou o processo pai*/ 
+	else if( pid > 0 ) 		// else, if i am a father process
 	{
-		char get_cpu[256], get_memory[256], kill[128];
-
-		sprintf(get_memory, "%s%d%s", "pmap ", pid, " | tail -n 1 | awk '{print $2}'");
-		sprintf(get_cpu, "%s%d%s", "ps -p ", pid, " -o %cpu | grep -v CPU ");
-		sprintf(kill, "%s%d","kill ",pid);
-
-		int i;
-		for (i = 0; i < 10; i++)
-		{	
-			printf("Time: %d second(s)\n", i);
-			printf("Memory Usage (Kb):\n");
-			system(get_memory);
-			printf("CPU Usage(%%):\n");
-			system(get_cpu);	//TODO guarde a cada segundo o consumo de memória (em Kilobytes) e CPU (em porcentagem) do processo filho
-			printf("\n");
-
-			sleep(1);
-		}
-
-		system(kill); //TODO após 10 segundos de execução, mate o proceso filho
-
+		father(pid);
 	}
-	else /* senão, sou o processo filho (pid == 0) */ 
+	else 	// else, if i am a child process (pid == 0)
 	{
-		if (strcmp(argv[1], "cpu") == 0) //TODO se argv[1] for igual a 'cpu', executar código com utilização intensa da UCP
-		{
-			while(1){}
-		}
-		else if (strcmp(argv[1], "cpu-mem") == 0) //TODO se argv[1] for igual a 'cpu-mem', executar código com utilização intensa da UCP e da memória
-		{
-			char *memory = (char *) malloc(sizeof(char));
-			while(1)
-			{
-				if (memory != NULL)
-				{
-					memory = (char *) malloc(sizeof(char));
-				}
-			}
-		}	
-
-
+		child(argv[1]);
 	}
 
-	perror ("Erro: ") ; /* execve nãoo funcionou */
+	perror ("Error: ") ; 	// status print
 
-	printf ("Tchau !\n") ;
-	exit(0) ; /* encerra o processo com sucesso (código 0) */
+	printf ("Bye!\n") ;
+	exit(0) ; 				// finish the process successfully (0 code)
 
 }
